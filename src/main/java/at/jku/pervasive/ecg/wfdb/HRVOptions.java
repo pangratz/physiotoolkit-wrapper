98754c285c972af20e527d6f6037aa6f4b1e1826
pangratz <cmueller.418@gmail.com>
2012-02-07 20:22:44 +0100

    implement start time, end time, rr file

--- src/main/java/at/jku/pervasive/ecg/wfdb/HRVOptions.java
+++ src/main/java/at/jku/pervasive/ecg/wfdb/HRVOptions.java
@@ -1,19 +1,224 @@
 package at.jku.pervasive.ecg.wfdb;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.joda.time.LocalTime;
 
 public class HRVOptions {
 
-  private final String annotation;
-  private final File file;
-  private LocalTime start;
+  private String file, annotation;
+  private double filt, hwin;
+  private String inputTimeFormat;
+  private boolean isRR;
+  private double lo1, hi1, lo2, hi2, lo3, hi3, lo4, hi4;
+  private String nnDiff;
+  private boolean outputMsec;
+  private boolean rrInMSec;
+  private boolean shortTermStats;
+  private LocalTime start, end;
 
   public HRVOptions(File file, String annotation) {
     super();
+    if (file != null) {
+      this.file = file.getAbsolutePath();
+    }
+    this.annotation = annotation;
+  }
+
+  public HRVOptions(String file) {
+    super();
+    this.file = file;
+    this.isRR = true;
+  }
+
+  public HRVOptions(String file, String annotation) {
+    super();
     this.file = file;
     this.annotation = annotation;
+  }
+
+  public String getAnnotation() {
+    return annotation;
+  }
+
+  public List<String> getCommand() {
+    List<String> cmd = new ArrayList<String>();
+    cmd.add("get_hrv");
+    if (isRR) {
+      cmd.add("-R");
+      cmd.add(file);
+    } else {
+      cmd.add(file);
+      cmd.add(annotation);
+    }
+    if (start != null) {
+      cmd.add(start.toString("HH:mm:ss"));
+    }
+    if (end != null) {
+      cmd.add(end.toString("HH:mm:ss"));
+    }
+    return cmd;
+  }
+
+  public LocalTime getEnd() {
+    return end;
+  }
+
+  public String getFile() {
+    return file;
+  }
+
+  public double getFilt() {
+    return filt;
+  }
+
+  public double getHi1() {
+    return hi1;
+  }
+
+  public double getHi2() {
+    return hi2;
+  }
+
+  public double getHi3() {
+    return hi3;
+  }
+
+  public double getHi4() {
+    return hi4;
+  }
+
+  public double getHwin() {
+    return hwin;
+  }
+
+  public String getInputTimeFormat() {
+    return inputTimeFormat;
+  }
+
+  public double getLo1() {
+    return lo1;
+  }
+
+  public double getLo2() {
+    return lo2;
+  }
+
+  public double getLo3() {
+    return lo3;
+  }
+
+  public double getLo4() {
+    return lo4;
+  }
+
+  public String getNnDiff() {
+    return nnDiff;
+  }
+
+  public LocalTime getStart() {
+    return start;
+  }
+
+  public boolean isOutputMsec() {
+    return outputMsec;
+  }
+
+  public boolean isRrInMSec() {
+    return rrInMSec;
+  }
+
+  public boolean isShortTermStats() {
+    return shortTermStats;
+  }
+
+  public void setAnnotation(String annotation) {
+    this.annotation = annotation;
+  }
+
+  public void setEnd(int m, int s) {
+    setEnd(new LocalTime(0, m, s));
+  }
+
+  public void setEnd(LocalTime end) {
+    this.end = end;
+    if (this.start == null) {
+      this.start = new LocalTime(0, 0, 0);
+    }
+  }
+
+  public void setFile(String file) {
+    this.file = file;
+  }
+
+  public void setFilt(double filt) {
+    this.filt = filt;
+  }
+
+  public void setHi1(double hi1) {
+    this.hi1 = hi1;
+  }
+
+  public void setHi2(double hi2) {
+    this.hi2 = hi2;
+  }
+
+  public void setHi3(double hi3) {
+    this.hi3 = hi3;
+  }
+
+  public void setHi4(double hi4) {
+    this.hi4 = hi4;
+  }
+
+  public void setHwin(double hwin) {
+    this.hwin = hwin;
+  }
+
+  public void setInputTimeFormat(String inputTimeFormat) {
+    this.inputTimeFormat = inputTimeFormat;
+  }
+
+  public void setLo1(double lo1) {
+    this.lo1 = lo1;
+  }
+
+  public void setLo2(double lo2) {
+    this.lo2 = lo2;
+  }
+
+  public void setLo3(double lo3) {
+    this.lo3 = lo3;
+  }
+
+  public void setLo4(double lo4) {
+    this.lo4 = lo4;
+  }
+
+  public void setNnDiff(String nnDiff) {
+    this.nnDiff = nnDiff;
+  }
+
+  public void setOutputMsec(boolean outputMsec) {
+    this.outputMsec = outputMsec;
+  }
+
+  public void setRrInMSec(boolean rrInMSec) {
+    this.rrInMSec = rrInMSec;
+  }
+
+  public void setShortTermStats(boolean shortTermStats) {
+    this.shortTermStats = shortTermStats;
+  }
+
+  public void setStart(int m, int s) {
+    setStart(new LocalTime(0, m, s));
+  }
+
+  public void setStart(LocalTime start) {
+    this.start = start;
   }
 
   public HRVOptions startTime(LocalTime start) {
--- src/test/java/at/jku/pervasive/ecg/wfdb/HRVOptionsTest.java
+++ src/test/java/at/jku/pervasive/ecg/wfdb/HRVOptionsTest.java
@@ -2,8 +2,55 @@ package at.jku.pervasive.ecg.wfdb;
 
 import java.io.File;
 import java.net.URISyntaxException;
+import java.util.List;
+
+import org.joda.time.LocalTime;
 
 public class HRVOptionsTest extends PhysioToolkitTestCase {
+
+  public void testEndTime() {
+    HRVOptions opts = new HRVOptions("/rrfile.rr");
+    opts.setStart(5, 42);
+    opts.setEnd(10, 11);
+
+    List<String> cmd = opts.getCommand();
+    assertEquals(5, cmd.size());
+    assertEquals("00:05:42", cmd.get(3));
+    assertEquals("00:10:11", cmd.get(4));
+  }
+
+  public void testEndTimeWithoutStartTime() {
+    HRVOptions opts = new HRVOptions("/rrfile.rr");
+    opts.setEnd(10, 11);
+
+    List<String> cmd = opts.getCommand();
+    assertEquals(5, cmd.size());
+    assertEquals("00:00:00", cmd.get(3));
+    assertEquals("00:10:11", cmd.get(4));
+  }
+
+  public void testInstantation() throws Exception {
+    File testFile = getFile("/test.dat");
+    testFile = new File(testFile.getParentFile(), "test");
+
+    HRVOptions options = new HRVOptions(testFile, "qrs");
+    options.setStart(10, 11);
+    options.setEnd(new LocalTime(1, 10, 11));
+    List<String> cmd = options.getCommand();
+
+    assertNotNull(cmd);
+    assertEquals(5, cmd.size());
+    assertTrue(cmd.contains(testFile.getAbsolutePath()));
+    assertTrue(cmd.contains("qrs"));
+
+    int fileIndex = cmd.indexOf(testFile.getAbsolutePath());
+    int annotationIndex = cmd.indexOf("qrs");
+
+    assertEquals("get_hrv", cmd.get(0));
+    assertTrue(fileIndex == annotationIndex - 1);
+    assertEquals("00:10:11", cmd.get(annotationIndex + 1));
+    assertEquals("01:10:11", cmd.get(annotationIndex + 2));
+  }
 
   public void testInvalidAnnotation() throws URISyntaxException {
     File testFile = getFile("/test.dat");
@@ -38,10 +85,36 @@ public class HRVOptionsTest extends PhysioToolkitTestCase {
 
   public void testNullFile() throws Exception {
     try {
-      new HRVOptions(null, "");
+      new HRVOptions((File) null, "");
     } catch (Exception e) {
       fail("should not throw an exception");
     }
+  }
+
+  public void testNullFileString() throws Exception {
+    try {
+      new HRVOptions((String) null, "");
+    } catch (Exception e) {
+      fail("should not throw an exception");
+    }
+  }
+
+  public void testRRFile() {
+    HRVOptions opts = new HRVOptions("rrfile.rr");
+
+    List<String> cmd = opts.getCommand();
+    assertEquals(3, cmd.size());
+    assertEquals("-R", cmd.get(1));
+    assertEquals("rrfile.rr", cmd.get(2));
+  }
+
+  public void testStartTime() {
+    HRVOptions opts = new HRVOptions("/rrfile.rr");
+    opts.setStart(10, 11);
+
+    List<String> cmd = opts.getCommand();
+    assertEquals(4, cmd.size());
+    assertEquals("00:10:11", cmd.get(3));
   }
 
 }
