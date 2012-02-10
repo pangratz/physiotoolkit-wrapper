ac566530cdf589a3b8a57ebf31c513fde564501e
pangratz <cmueller.418@gmail.com>
2012-02-10 14:36:54 +0100

    add test for null argument

--- src/test/java/at/jku/pervasive/ecg/wfdb/Edf2MitTest.java
+++ src/test/java/at/jku/pervasive/ecg/wfdb/Edf2MitTest.java
@@ -2,4 +2,14 @@ package at.jku.pervasive.ecg.wfdb;
 
 public class Edf2MitTest extends PhysioToolkitTestCase {
 
+  public void testEdf2MitWithNullParameter() {
+    try {
+      physioToolkit.edf2mit(null);
+      fail("should throw an exception");
+    } catch (IllegalArgumentException e) {
+    } catch (Exception e) {
+      fail("should throw an IllegalArgumentException");
+    }
+  }
+
 }
