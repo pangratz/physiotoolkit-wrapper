5e8f14b45f4cd60f05fd6bc0487a4f0a237c4c8d
pangratz <cmueller.418@gmail.com>
2012-02-13 16:55:04 +0100

    add more HRV attributes

--- src/main/java/at/jku/pervasive/ecg/wfdb/HRV.java
+++ src/main/java/at/jku/pervasive/ecg/wfdb/HRV.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class HRV {
 
-  private double AVNN, SDNN, NNRR, rMSSD, pNN20, pNN50, TOT_PWR, VLF_PWR,
-      LF_PWR, HF_PWR, LFHF;
+  private double AVNN, SDNN, SDANN, SDNNIDX, NNRR, rMSSD, pNN20, pNN50,
+      TOT_PWR, ULF_PWR, VLF_PWR, LF_PWR, HF_PWR, LFHF;
 
   public HRV(String getHrvResult) {
     super();
@@ -45,12 +45,24 @@ public class HRV {
     return rMSSD;
   }
 
+  public double getSDANN() {
+    return SDANN;
+  }
+
   public double getSDNN() {
     return SDNN;
   }
 
+  public double getSDNNIDX() {
+    return SDNNIDX;
+  }
+
   public double getTOT_PWR() {
     return TOT_PWR;
+  }
+
+  public double getULF_PWR() {
+    return ULF_PWR;
   }
 
   public double getVLF_PWR() {
@@ -60,15 +72,56 @@ public class HRV {
   private void parse(String string) {
     Scanner s = new Scanner(string);
     while (s.hasNextLine()) {
-      String l = s.nextLine();
-      String[] split = l.split(" = ");
-      if (split[0].trim().equals("SDNN")) {
-        String dS = split[1];
-        this.SDNN = Double.parseDouble(dS);
-      } else if (split[0].trim().equals("AVNN")) {
-        this.AVNN = Double.parseDouble(split[1]);
-      }
+      processLine(s.nextLine());
     }
   }
 
+  private void processLine(String line) {
+    Scanner s = new Scanner(line);
+    s.useDelimiter(" = ");
+    if (s.hasNext()) {
+      String name = s.next().trim();
+      if (s.hasNext()) {
+        String valueStr = s.next().trim();
+        double value = Double.parseDouble(valueStr);
+
+        /**
+         * next: beautiful String switch
+         * 
+         * we're using JDK6 and the String switch statement has been made
+         * available in JDK7 - 16 years after requesting it:
+         * http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=1223179
+         **/
+        if ("AVNN".equals(name)) {
+          this.AVNN = value;
+        } else if ("SDNN".equals(name)) {
+          this.SDNN = value;
+        } else if ("SDANN".equals(name)) {
+          this.SDANN = value;
+        } else if ("SDNNIDX".equals(name)) {
+          this.SDNNIDX = value;
+        } else if ("NN/RR".equals(name)) {
+          this.NNRR = value;
+        } else if ("rMSSD".equals(name)) {
+          this.rMSSD = value;
+        } else if ("pNN20".equals(name)) {
+          this.pNN20 = value;
+        } else if ("pNN50".equals(name)) {
+          this.pNN50 = value;
+        } else if ("TOT PWR".equals(name)) {
+          this.TOT_PWR = value;
+        } else if ("ULF PWR".equals(name)) {
+          this.ULF_PWR = value;
+        } else if ("VLF PWR".equals(name)) {
+          this.VLF_PWR = value;
+        } else if ("LF PWR".equals(name)) {
+          this.LF_PWR = value;
+        } else if ("HF PWR".equals(name)) {
+          this.HF_PWR = value;
+        } else if ("LF/HF".equals(name)) {
+          this.LFHF = value;
+        }
+      }
+    }
+  }
 }
